b8318701e624f3e290016d8222721cc2e6536808
Cralley <jkolecr@siu.edu>
2019-07-24 14:37:47 -0500

    Adds windows support for test files

--- MTL_Tests_Bardh.py
+++ MTL_Tests_Bardh.py
@@ -2,37 +2,42 @@ from MTL import *
 import time
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
-from multiprocessing import Pool
+from multiprocessing import Pool, freeze_support
+import os
 
+if __name__ == '__main__':
+    if os.name == 'nt':    
+        freeze_support()
+    
+    Aspeed = 1
+    bspeed = 120
 
-Aspeed = 1
-bspeed = 120
+    Arpm = 1
+    brpm = 4500
 
-Arpm = 1
-brpm = 4500
+    speed_pred = Predicate('speed',Aspeed,bspeed)
+    rpm_pred = Predicate('rpm',Arpm,brpm)
+    root = Not(And(Finally(0,100,speed_pred), Finally(0,100,rpm_pred)))
+    #root = Until(0,float('inf'),speed_pred,rpm_pred)
+    # root = Finally(1,2.2,Predicate('geese',-1,-1))
 
-speed_pred = Predicate('speed',Aspeed,bspeed,False)
-rpm_pred = Predicate('rpm',Arpm,brpm,False)
-root = Not(And(Finally(0,100,speed_pred), Finally(0,100,rpm_pred)))
-#root = Until(0,float('inf'),speed_pred,rpm_pred)
-# root = Finally(1,2.2,Predicate('geese',-1,-1))
+    data = genfromtxt('data.csv', delimiter=',')
+    time_data = genfromtxt('dataTime.csv')
+    #two_dim_pred = Predicate('data',[[-1.0,1.0],[1.0,1.0]],[-120.0,4500.0])
+    #root = two_dim_pred
 
-data = genfromtxt('data.csv', delimiter=',')
-time_data = genfromtxt('dataTime.csv')
-#two_dim_pred = Predicate('data',[[-1.0,1.0],[1.0,1.0]],[-120.0,4500.0])
-#root = two_dim_pred
+    speedData = np.transpose(data[:,0])
 
-speedData = np.transpose(data[:,0])
+    rpmData = np.transpose(data[:,1])
 
-rpmData = np.transpose(data[:,1])
+    timeData = np.transpose(time_data)
 
-timeData = np.transpose(time_data)
-
-traces = {'speed':speedData,'rpm':rpmData}
-#traces = {'data' : data}
-time_stamps = timeData
-t0 = time.time()
-root.eval_interval(traces,time_stamps)
-t1 = time.time()
-print('Robustness: ',root.robustness)
-print('Time: ', t1 - t0)
+    traces = {'speed':speedData,'rpm':rpmData}
+    #traces = {'data' : data}
+    time_stamps = timeData
+    t0 = time.time()
+    root.eval_interval(traces,time_stamps)
+    t1 = time.time()
+    print('Robustness: ',root.robustness)
+    print('Time: ', t1 - t0)
+    
