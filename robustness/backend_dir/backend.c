5246dc53614d0b4b4fc9dd9374cd0f1552ed6673
nogthar <jkolecr@gmail.com>
2019-09-28 16:17:56 -0500

    C cpu backend higher dim function added

--- robustness/backend_dir/backend.c
+++ robustness/backend_dir/backend.c
@@ -4,6 +4,55 @@
 #include <math.h>
 #include "backend.h"
 #include <omp.h>
+#include <stdbool.h>
+#include "osqp.h"
+
+c_float higher_dim_pred(c_int n, c_int m, c_float* p_values, c_int* p_indexs, c_int* p_pointers,c_int p_nnz,c_float* q,c_float* A_values,c_int* A_indexs, c_int* A_pointers,c_int A_nnz,c_float* l, c_float* u){
+    OSQPData* data;
+    OSQPSettings  *settings;
+    OSQPWorkspace *work;
+          
+    if((data = (OSQPData *)c_malloc(sizeof(OSQPData))) < 0)
+        perror("MEM ERROR");
+    
+    if((settings = (OSQPSettings *)c_malloc(sizeof(OSQPSettings))) < 0)
+        perror("MEM ERROR");
+        
+    if (data) {
+        data->n = n;
+        data->m = m;
+        data->P = csc_matrix(data->n, data->n, p_nnz, p_values, p_indexs, p_pointers);
+        data->q = q;
+        data->A = csc_matrix(data->m, data->n, A_nnz, A_values, A_indexs, A_pointers);
+        data->l = l;
+        data->u = u;
+    }
+    
+    if (settings) {
+        osqp_set_default_settings(settings);
+        settings->verbose = false;
+    }
+    
+    
+    osqp_setup(&work, data, settings);
+   
+    osqp_solve(work);
+   
+   c_float objective_value = work->info->obj_val;
+   
+   if (data) {
+        if (data->A) c_free(data->A);
+        if (data->P) c_free(data->P);
+        c_free(data);
+    }
+    
+    if (settings) 
+        c_free(settings);
+    
+    return objective_value;
+}
+
+
 
 void  c_not(float* robustness,long length){
     long i;
@@ -30,28 +79,6 @@ void c_and(float* left_robustness, float* right_robustness, long length){
     }
 }
 
-/*
-long search_sorted(float* time_stamps,float time,long start_lower_index,long length){
-    long lower_index = start_lower_index;
-    long upper_index = length;
-    long mid_index;
-    int flag = 0;
-    while(flag == 0){
-        mid_index = (lower_index + upper_index) / 2; //Rounds down to lower index if odd
-        if(time < *(time_stamps + mid_index)) {
-            upper_index = mid_index;
-        }
-        else if(*(time_stamps + mid_index) < time){
-            lower_index = mid_index;
-        }
-        else{
-            flag = 1;
-        }
-    }
-    return mid_index;
-}*/
-
-// search sorted attempt (Rania 8/1)
 
 long search_sorted(float* time_stamps,float time,long start_lower_index,long length){
     long lower_index = start_lower_index;
@@ -69,8 +96,8 @@ long search_sorted(float* time_stamps,float time,long start_lower_index,long len
             upper_index = middle - 1;
 
         middle = (lower_index + upper_index) /  2;
-   	}
-   	return middle;
+    }
+    return middle;
 }
 float max(float left, float right){
     if(left > right){
@@ -117,53 +144,53 @@ long find_max(float* array, long start_index, long end_index){
 
 
 float* c_finally_threaded(float lower_time_bound, float upper_time_bound, float* robustness, float* time_stamps, long length){
-	long i;
-	float max;
-	float* finally_robustness = (float*) malloc(length * sizeof(float));
-	if(finally_robustness == NULL){
-		perror("Error: finally could not malloc memory");
-		exit(-1);
-	}
+    long i;
+    float max;
+    float* finally_robustness = (float*) malloc(length * sizeof(float));
+    if(finally_robustness == NULL){
+        perror("Error: finally could not malloc memory");
+        exit(-1);
+    }
 
-	if(lower_time_bound == 0 && isinf(upper_time_bound)){
-		max = *(robustness + (length - 1));
-		for(i = length - 1; i >= 0; i--){
-		    if(*(robustness + i) > max){
-			max = *(robustness + i);
-		    }
-		    *(finally_robustness + i) = max;
-		}
-	}
-	else{
-		long current_time_step;
-		#pragma omp parallel 
-		#pragma taskloop num_tasks(32)
-		for(current_time_step = length - 1; current_time_step >= 0; current_time_step--){
-		    float lower_bound = *(time_stamps + current_time_step) + lower_time_bound;
-		    float upper_bound = *(time_stamps + current_time_step) + upper_time_bound;
-		    //long search_sorted(float* time_stamps,float time,long start_lower_index,long length)
-		    long upper_bound_index = search_sorted(time_stamps,upper_bound,current_time_step,length);
-		    long lower_bound_index; 
-		    
-		    if(lower_time_bound == 0){
-		        lower_bound_index = current_time_step;
-		    }
-		    else{
-		        lower_bound_index = search_sorted(time_stamps,lower_bound,current_time_step,length);
-		    }
-		    
-		    if(lower_bound_index == upper_bound_index){
-		        *(finally_robustness + current_time_step) = *(robustness + lower_bound_index);
-		    }
-		    else{
-		        long max_index = find_max(robustness,lower_bound_index,upper_bound_index);
-		        *(finally_robustness + current_time_step) = *(robustness + max_index);
-		    }
-		
-		}
-	}
-	
-	return finally_robustness;
+    if(lower_time_bound == 0 && isinf(upper_time_bound)){
+        max = *(robustness + (length - 1));
+        for(i = length - 1; i >= 0; i--){
+            if(*(robustness + i) > max){
+            max = *(robustness + i);
+            }
+            *(finally_robustness + i) = max;
+        }
+    }
+    else{
+        long current_time_step;
+        #pragma omp parallel 
+        #pragma taskloop num_tasks(32)
+        for(current_time_step = length - 1; current_time_step >= 0; current_time_step--){
+            float lower_bound = *(time_stamps + current_time_step) + lower_time_bound;
+            float upper_bound = *(time_stamps + current_time_step) + upper_time_bound;
+            //long search_sorted(float* time_stamps,float time,long start_lower_index,long length)
+            long upper_bound_index = search_sorted(time_stamps,upper_bound,current_time_step,length);
+            long lower_bound_index; 
+            
+            if(lower_time_bound == 0){
+                lower_bound_index = current_time_step;
+            }
+            else{
+                lower_bound_index = search_sorted(time_stamps,lower_bound,current_time_step,length);
+            }
+            
+            if(lower_bound_index == upper_bound_index){
+                *(finally_robustness + current_time_step) = *(robustness + lower_bound_index);
+            }
+            else{
+                long max_index = find_max(robustness,lower_bound_index,upper_bound_index);
+                *(finally_robustness + current_time_step) = *(robustness + max_index);
+            }
+        
+        }
+    }
+    
+    return finally_robustness;
 }
 float* c_finally(float lower_time_bound, float upper_time_bound, float* robustness, float* time_stamps, long length){
     long i;
@@ -285,8 +312,8 @@ float* c_global_threaded(float lower_time_bound, float upper_time_bound, float* 
     }
     else{
         long current_time_step;
-	#pragma omp parallel 
-	#pragma taskloop num_tasks(32)
+    #pragma omp parallel 
+    #pragma taskloop num_tasks(32)
         for(current_time_step= length - 1; current_time_step >= 0; current_time_step--){
             float lower_bound = *(time_stamps + current_time_step) + lower_time_bound;
             float upper_bound = *(time_stamps + current_time_step) + upper_time_bound;
@@ -334,8 +361,8 @@ float* c_until(float lower_time_bound, float upper_time_bound, float* left_robus
     else{
         long current_time_step;
         float last_robustness = -INFINITY;
-	#pragma omp parallel 
-	#pragma taskloop num_tasks(32)
+    #pragma omp parallel 
+    #pragma taskloop num_tasks(32)
         for(current_time_step = length-1; current_time_step >= 0; current_time_step--){
             float lower_bound = *(time_stamps + current_time_step) + lower_time_bound;
             float upper_bound = *(time_stamps + current_time_step) + upper_time_bound;
