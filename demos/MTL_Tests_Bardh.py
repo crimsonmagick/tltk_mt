Fixed a memory leak and finished  implimenting neg robustness calculation

demos/MTL_Tests_Bardh.py
@@ -45,17 +45,17 @@ if __name__ == '__main__':
     # traces = {'speed': speedData, 'rpm': rpmData}
     # #traces = {'data' : data}
     # time_stamps = timeData
-    i = 1000000
+    i = 10
     #Acomb = [[1,0],[0,1]]
     #bcomb = [150, 4500]
-    Acomb = [1,0]
+    Acomb = [1,1]
     bcomb = [150]
     #root = MTL.Not(MTL.And(MTL.Finally(0,float('inf'),MTL.Predicate('comb',Acomb,bcomb)),MTL.Finally(0,float('inf'),MTL.Predicate('comb',Acomb,bcomb))))
     #plt.scatter(timeData, combData[1])
     #plt.show()
     #root = MTL.Not(MTL.And(MTL.Finally(0,float('inf'),MTL.Predicate('comb',Acomb,bcomb)),MTL.Finally(0,float('inf'),MTL.Predicate('comb',Acomb,bcomb))))
-    traces = {'comb': [[10,0]]*i}
-    root = MTL.Not(MTL.Finally(0,100,MTL.Predicate('comb', Acomb, bcomb),mode))
+    traces = {'comb': [[175,54]]*i}
+    root = MTL.Finally(0,100,MTL.Predicate('comb', Acomb, bcomb))
     #traces = {'comb': combData.tolist()}
     time_stamps = np.arange(1, i + 1)
     