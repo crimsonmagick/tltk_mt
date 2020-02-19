2f0b13256a3638626fe93e80dc56ec2df79c65cd
Bardh Hoxha (TNA) <bardh.hoxha@toyota.com>
2020-02-19 11:19:48 -0500

    added specs for experimental results

--- demos/Phi_4.py
+++ demos/Phi_4.py
@@ -5,38 +5,32 @@ import MTL as MTL
 import numpy as np
 import time
 
-
-
 list_range = (2**i for i in range(10,31))
-
 
 mode = 'cpu_threaded'
 
-
 for i in list_range:
-    Ar3 = [1, 0]
-
-    br3 = [160]
     
-    Ar4 = 1
-    br4 = 4500
+    Ar7 = [[2.5 -0.6],[[0.1 -0.08]],[[-1.9 -0.4]],[1.7 0.8]]
+    br7 = [[5.5],[3.8],[4.9],[7.5]]
     
+    Ar8 = [[2.5 -0.6],[[0.1 -0.08]],[[-1.9 -0.4]],[1.7 0.8]]
+    br8 = [[4.5],[2.8],[2.9],[2.5]]
+    
+    r7 = MTL.Predicate('data1',Ar3,br3)
+    r8 = MTL.Predicate('data2',Ar4,br4)
 
-    pred3 = MTL.Predicate('data1',Ar3,br3)
-    pred4 = MTL.Predicate('data2',Ar4,br4)
-
-    #traces = {'data1': np.ones(i,dtype=np.float32),'data2': np.ones(i,dtype=np.float32)}
-    traces = {'data1': [[1,1]]*i,'data2': np.ones(i,dtype=np.float32)}
+    traces = {} 
+    traces['data1'] = [[1,1,1]]*i
+    traces['data2'] = [[1,1,1]]*i
+    #traces = {'data1': [[1,1,1]]*i,dtype=np.float32),'data2': [[1,1,1]]*i,dtype=np.float32)}
     time_stamps = np.arange(1, i + 1,dtype=np.float32)
-
-    #root = MTL.Not(MTL.And(MTL.Global(0,100,pred3),MTL.Global(0,100,pred4)))
-    #root = MTL.Global(0,1000,pred3,'cpu_threaded')
-
-    root = MTL.Global(0,1000,pred4,"cpu_threaded")
-
+    
+    root = MTL.Global(0,float('inf'), MTL.And(r7,MTL.Finally(0,100,r8,mode),mode))
+            
     t0 = time.time()
     root.eval_interval(traces, time_stamps)
     t1 = time.time()
-    print("Phi_1_higher_dim\t","| Mode:" ,mode,'\t| Samples:', i, '\t| Time: ', '%.4f'%(t1 - t0), '    \t| robustness', root.robustness)
+    print("phi_2\t","| Mode:" ,mode,'\t| Samples:', "{:,}".format(i), '\t| Time: ', '%.4f'%(t1 - t0), '    \t| robustness', root.robustness)
     del traces
     del time_stamps
