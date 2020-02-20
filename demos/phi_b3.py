faaf9d839296e806c4c5fd2308e30784481a216f
Bardh Hoxha (TNA) <bardh.hoxha@toyota.com>
2020-02-20 10:13:00 -0500

    added 3 specs for comparison with breach (potentially we can add 3 more). Note that the 3rd spec is returning a robustness of 8995 and I suspect that something is wrong with the code. Please check.

--- demos/phi_b3.py
+++ demos/phi_b3.py
@@ -24,11 +24,11 @@ for i in list_range:
     pred3 = MTL.Predicate('data1',Ar3,br3)
     pred4 = MTL.Predicate('data2',Ar4,br4)
 
-    traces = {'data1': np.ones(i,dtype=np.float32),'data2': np.ones(i,dtype=np.float32)}
+    traces = {'data1': np.ones(i,dtype=np.float32)*5,'data2': np.ones(i,dtype=np.float32)*5}
     time_stamps = np.arange(1, i + 1,dtype=np.float32)
     
-    root = MTL.Not(MTL.Finally(0,100,MTL.And(pred3,pred4,mode),mode),mode)
-        
+    root = MTL.Not(MTL.And(MTL.Finally(0,float('inf'),pred3,mode),MTL.Global(0,float('inf'),MTL.And(pred4, MTL.Global(0,float('inf'),MTL.Finally(0,float('inf'),MTL.And(pred3,MTL.Until(0,float('inf'),pred3,pred4,mode),mode),mode),mode),mode),mode),mode),mode)
+    
     t0 = time.time()
     root.eval_interval(traces, time_stamps)
     t1 = time.time()
@@ -36,7 +36,7 @@ for i in list_range:
     x_length.append(i)
     y_time.append(t1-t0)
     
-    print("phi_1\t","| Mode:" ,mode,'\t| Samples:', "{:,}".format(i), '\t| Time: ', '%.4f'%(t1 - t0), '    \t| robustness', root.robustness)
+    print("phi_b3\t","| Mode:" ,mode,'\t| Samples:', "{:,}".format(i), '\t| Time: ', '%.4f'%(t1 - t0), '    \t| robustness', root.robustness)
     del traces
     del time_stamps
 
